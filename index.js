ed099893924c0c317f8c56b4ed7266ffee97813c
bishar <bishar.dahir@student.moringaschool.com>
2023-12-16 18:10:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+  if (typeof customerName === 'string') {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(value) {
+  bestCustomer = value;
+}
+
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
